47ec5020544eb15697f375a503aa8b265971c5c3
DHVSGG <diego@qapla.gg>
2020-07-23 12:52:49 -0500

    Update sendbird SDK key

--- src/utilities/Constants.js
+++ src/utilities/Constants.js
@@ -1,4 +1,4 @@
 export const IOS_BUNDLE_ID = 'org.Qapla.QaplaApp';
 export const ANDROID_PACKAGE_NAME = 'com.qapla.gaming.app';
 export const APPSTORE_ID = '1485332229';
-export const SENDBIRD_KEY = '8117CF1A-FE70-4412-9DEB-FD05AF5CA7F8';
+export const SENDBIRD_KEY = '7E54A9F8-DCE3-420E-8DC8-D3E8B081A2B0';
