9ed4ebf61849036d64db13c8216075d4f75b1de3
DHVSGG <diego@qapla.gg>
2020-12-16 16:36:16 -0600

    Add copy icon on code field

--- src/components/CreateInvitation/CreateInvitation.jsx
+++ src/components/CreateInvitation/CreateInvitation.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { Container, Grid, Button } from '@material-ui/core';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+
 import { getRandomString } from '../../utils/utils';
 import { invitationCodeExists, saveInvitationCode } from '../../services/database';
+import QaplaTextField from '../QaplaTextField/QaplaTextField';
 
 const CreateInvitation = () => {
     const [invitationCode, setInvitationCode] = useState('');
@@ -10,10 +13,23 @@ const CreateInvitation = () => {
         const invitationCode = getRandomString();
         if (!(await invitationCodeExists(invitationCode))) {
             saveInvitationCode(invitationCode);
-            return setInvitationCode(`https://qapplaapp.web.app/createAccount/${invitationCode}`);
+            return setInvitationCode(invitationCode);
         } else {
             // If the code exists call this function until it generates a new valid code
             return generateCode();
+        }
+    }
+
+    /**
+     * Save the EventId on the user clipboard
+     */
+    const copyInvitationCode = () => {
+        if (invitationCode !== '') {
+            const field = document.getElementById('InvitationCodeTextField');
+            field.value = invitationCode;
+            field.select();
+            document.execCommand('copy');
+            alert('Texto copiado');
         }
     }
 
@@ -28,9 +44,17 @@ const CreateInvitation = () => {
                         onClick={generateCode}>
                         Generar link de invitación
                     </Button>
-                    <p>
-                        {invitationCode}
-                    </p>
+                    <Grid item md={12}>
+                        <br/>
+                        <QaplaTextField
+                            disabled={invitationCode === ''}
+                            label='Codigo de invitación'
+                            value={invitationCode}
+                            inputAdornment={<FileCopyIcon />}
+                            onChange={() => {}}
+                            onPressAdornment={copyInvitationCode}
+                            id='InvitationCodeTextField' />
+                    </Grid>
                 </Grid>
             </Grid>
         </Container>
